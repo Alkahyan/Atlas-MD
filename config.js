ac9364159dd144106e2456d9f845e7cec153306f
Alkahyan <128998518+Alkahyan@users.noreply.github.com>
2023-07-10 09:51:39 +0300

    Update config.js

--- config.js
+++ config.js
@@ -35,7 +35,7 @@ require("./BotCharacters.js");
 require("dotenv").config();
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "201275658375";   // You can Change this number //
+  gg = "918101187835";   // You can Change this number //
 }
 
 // -------------------------------------------------------------- //
